080ad37ca1116fc7d43136daff492c6ece867465
SandeepPanday2501 <pandayshiroyasha@gmail.com>
2023-02-28 10:22:41 +0545

    class

---
+++ MainClass/src/MainClass.java
@@ -0,0 +1,12 @@
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println("YOO!!");
+        String x = new String();
+        Shape object = new Shape();
+        System.out.println(object.width);
+
+        object.area();
+
+
+    }
+}
---
+++ MainClass/src/Shape.java
@@ -0,0 +1,9 @@
+public class Shape {
+   public int width =10;
+   public int height =20;
+   public void area(){
+      System.out.println("What is up?");
+      System.out.println("Area =" + (width * height));
+   }
+
+}
